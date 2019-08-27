7a816ae5a9bbf82ff7f45072910e66c353e07abc
Viktorija Valsoe <johndoe@example.com>
2019-08-27 15:26:10 +0200

    installed sass, created sass structure, moved App component to components, installed router

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>App is working</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
