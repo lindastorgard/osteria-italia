34659a856918eb3d51d07a5cb707f00245d43c56
Ida Bennetsen <idaelisabethb@gmail.com>
2019-08-27 23:18:47 -0400

    Use axios to get the bookings from the api

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import axios from 'axios';
+
 class Admin extends React.Component {
+
+    constructor(props: any) {
+        super(props)
+        this.getBookings();
+    }
+
+    getBookings = () => {
+        axios.get('http://localhost:8888/api/bookings/read.php')
+            .then(response => {
+                    console.log('Got response from server');
+                    console.log(response.data);
+            })
+            .catch(error => console.log(error));
+    }
     render() {
         return (
             <div>
