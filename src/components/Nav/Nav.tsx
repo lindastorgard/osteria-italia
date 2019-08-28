d0f0d911218a26edd1392e736c49a918bf82eee8
Viktorija Valsoe <johndoe@example.com>
2019-08-28 11:52:25 +0200

    Created navigation and routing to Booking,Admin and Menu componentns

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 class Nav extends React.Component {
     render() {
         return (
-            <div>
-                <p>Hello Nav World!</p>
-            </div>
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+					<a href="/" className="navbar-brand">Osteria Italia</a>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav mr-auto">
+							<li className="nav-item"><Link to={'/menu'} className="nav-link">Menu</Link></li>
+							<li className="nav-item"><Link to={'/booking'} className="nav-link">Reservations</Link></li>	
+						</ul>
+						<hr />
+					</div>
+				</nav> 
         )
     }
 }
