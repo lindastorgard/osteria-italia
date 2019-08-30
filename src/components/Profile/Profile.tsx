c06955b545529bb32f6dddbc764cf53dffeb3dec
micsson <michaelperssonmail@gmail.com>
2019-08-30 15:17:40 +0200

    export interface for profile and add form with working submit

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,25 +2,43 @@ import React, { Component } from 'react';
 import './Profile.scss';
 import { IBooking } from '../Booking/Booking';
 
+export interface IAddProfileState{
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+}
 
 export interface IAddProfileProps{
     theBooking: IBooking;
-    onchange(updatedBooking: IBooking): void,
+    onsubmit(updatedBooking: IBooking): void,
 }
 
-class Profile extends React.Component <IAddProfileProps,{}> {
+class Profile extends React.Component <IAddProfileProps,IAddProfileState> {
     constructor(props:any){
         super(props);
         
         this.handleInput = this.handleInput.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handleInput = (event: any) => { 
+        event.preventDefault();
         let booking = this.props.theBooking;
-        booking.profile = event.target.value;
+        booking.profile = this.state;
 
-        this.props.onchange(booking);
+        this.props.onsubmit(booking);
     }
+
+    handleInputChange(event:any) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        
+        this.setState({
+          [name]: value
+        } as any);
+      }
 
     render() {
 
@@ -28,11 +46,12 @@ class Profile extends React.Component <IAddProfileProps,{}> {
             <main className="profilePageContainer">
                 <div className="profileParent">
                 <h1>Your details</h1>
-                <form onChange={this.handleInput} className="profileChild">
-                    <input  className="profilebox" type="name" placeholder="Firstname"></input>
-                    <input  className="profilebox" type="lastname" placeholder="Lastname"></input>
-                    <input  className="profilebox" type="email" placeholder="Email"></input>
-                    <input  className="profilebox" type="phone" placeholder="Phone"></input>
+                <form onSubmit={this.handleInput} className="profileChild">
+                    <input name="firstName" className="profilebox" type="name" placeholder="Firstname" onChange={this.handleInputChange}></input>
+                    <input name="lastName" className="profilebox" type="lastname" placeholder="Lastname" onChange={this.handleInputChange}></input>
+                    <input name="email" className="profilebox" type="email" placeholder="Email" onChange={this.handleInputChange}></input>
+                    <input name="phone" className="profilebox" type="phone" placeholder="Phone" onChange={this.handleInputChange}></input>
+                    <input type="submit" value="Submit" />
                 </form>
                 </div>
             </main>
