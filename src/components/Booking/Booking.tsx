9ac92c892847f74c6b20fc8326bb1f06f9b9cb88
Viktorija Valsoe <johndoe@example.com>
2019-09-06 13:46:09 +0200

    changed indentation to 2 instead of 4 spaces

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -11,102 +11,100 @@ import Time from '../Time/Time';
 import { any } from 'prop-types';
 
 export interface IBooking{
-    view: number,
-    guests: number,
-    date: Date,
-    time: string,
-    profile: IAddProfileState
+  view: number,
+  guests: number,
+  date: Date,
+  time: string,
+  profile: IAddProfileState
 }
 
 interface IBookingState {
-
-    booking: IBooking;
+	booking: IBooking;
 }
 
 class Booking extends Component <{}, IBookingState> {
-    constructor(props:any){
-        super(props);
+	constructor(props:any){
+    super(props);
 
-        // set booking state
-        this.state = {
+      // set booking state
+      this.state = {
 
-            booking: {
-                view: 1,
-                guests: 0,
-                date: new Date(),
-                time: '',
-                profile: {
-                    firstName:'',
-                    lastName: '',
-                    email: '',
-                    phone: '',
-                    firstNameError: '',
-                    lastNameError: '',
-                    emailError: '',
-                    phoneError: '',
-                    showFirstNameError: false,
-                    showLastNameError: false,
-                    showEmailError: false,
-                    showPhoneError: false
-                }
-                
-            }
-
-        };
-        this.updateState = this.updateState.bind(this);
-        this.makeBooking = this.makeBooking.bind(this);
+        booking: {
+          view: 1,
+          guests: 0,
+          date: new Date(),
+          time: '',
+          profile: {
+            firstName:'',
+            lastName: '',
+            email: '',
+            phone: '',
+            firstNameError: '',
+            lastNameError: '',
+            emailError: '',
+            phoneError: '',
+            showFirstNameError: false,
+            showLastNameError: false,
+            showEmailError: false,
+            showPhoneError: false
+          }        
+        }
+			};
+			
+      this.updateState = this.updateState.bind(this);
+      this.makeBooking = this.makeBooking.bind(this);
 
     }
 
-    // Handle fields chenge
+    // Handle fields change
     updateState(updatedBooking: IBooking) {
-        this.setState({
-            booking: updatedBooking
-        });
+      this.setState({
+      	booking: updatedBooking
+      });
     }
 
     // make booking
     makeBooking = () => {
       axios.post('http://localhost:8888/booking_api/api/bookings/createBooking.php', {
-        customer_id: 5,
-        guest_nr: this.state.booking.guests,
-        date: "2019-08-05 18.00.00"
+      	customer_id: 5,
+      	guest_nr: this.state.booking.guests,
+      	date: "2019-08-05 18.00.00"
       })
-          .then(response => {
-                  console.log(response.data.message);
-          })
-          .catch(error => console.log(error.data.message));
-  };
+        .then(response => {
+          console.log(response.data.message);
+      	})
+        	.catch(error => console.log(error.data.message));
+  	};
 
-  render() {
-    switch(this.state.booking.view){
-      case 1:
-        return(
-          <div>
-            <Guests onclick={this.updateState} theBooking={this.state.booking}/>
-          </div>
-        )
+  	render() {
+    	switch(this.state.booking.view){
+      	case 1:
+        	return(
+          	<div>
+            	<Guests onclick={this.updateState} theBooking={this.state.booking}/>
+          	</div>
+        	)
 
         case 2:
-                return(
-                    <div>
-                        <Calender onDayClick={this.updateState} theBooking={this.state.booking}/>
-                    </div>
-                )
+          return(
+            <div>
+              <Calender onDayClick={this.updateState} theBooking={this.state.booking}/>
+            </div>
+          )
 
         case 3:
-                return(
-                    <div>
-                      <Time onclick={this.updateState} theBooking={this.state.booking}/>
-                    </div>
-                )
+          return(
+            <div>
+              <Time onclick={this.updateState} theBooking={this.state.booking}/>
+            </div>
+          )
 
         case 4:
-                return(
-                    <div>
-                      <Profile onsubmit={this.updateState} onclick={this.updateState} theBooking={this.state.booking}/>
-                    </div>
-                )
+          return(
+            <div>
+              <Profile onsubmit={this.updateState} onclick={this.updateState} theBooking={this.state.booking}/>
+            </div>
+          )
 
         case 5:
           return(
@@ -114,8 +112,7 @@ class Booking extends Component <{}, IBookingState> {
               <Summary onclick={this.updateState} makesubmit={this.makeBooking} theBooking={this.state.booking}/>
             </div>
           )
-
-    }
-  }
-}
+    	}
+  	}
+	}
 export default Booking;
