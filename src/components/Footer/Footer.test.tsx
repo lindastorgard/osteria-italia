b00c4f8ea48677aa07892cb7176cef4fc6181de8
micsson <michaelperssonmail@gmail.com>
2019-09-10 15:05:36 +0200

    test that footer renders two social icons

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -10,3 +10,8 @@ configure({ adapter: new Adapter() });
 it('renders without crashing', () => {
   let wrapper = shallow(<Footer />);
 });
+
+it('renders two `.social-icon`', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find('.social-icon')).toHaveLength(2);
+  });
