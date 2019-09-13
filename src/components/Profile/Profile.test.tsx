9f5238d97d804d7bd217ea8a073710396cfd4a68
micsson <michaelperssonmail@gmail.com>
2019-09-13 15:04:55 +0200

    add Sebastians solution for checking if customer already exist in database

--- src/components/Profile/Profile.test.tsx
+++ src/components/Profile/Profile.test.tsx
@@ -4,11 +4,18 @@ import Profile from './Profile';
 
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { IAddProfileProps } from './Profile';
+// import { IAddProfileProps } from './Profile';
 
 configure({ adapter: new Adapter() });
 
-const props: IAddProfileProps = {
+const props = {
+  newCustomer: {
+    firstName: 'Michael',
+      lastName: 'Persson',
+      email: 'Michaelperssonl@gmail.com',
+      phone: '12345678',
+  },
+
   theBooking:{
     view: 2,
     guests: 4,
@@ -16,10 +23,10 @@ const props: IAddProfileProps = {
     time: '18:00',
     customerId: 5,
     profile: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: '',
+      firstName: 'Michael',
+      lastName: 'Persson',
+      email: 'Michaelperssonmail@gmail.com',
+      phone: '12345678',
 
       firstNameError: '',
       lastNameError: '',
@@ -35,8 +42,12 @@ const props: IAddProfileProps = {
       myCustomers: []
     }
   },
-    onsubmit: jest.fn(),
-    onclick: jest.fn()
+
+    isCustomerInDatabase: jest.fn(),
+    handleSubmit: jest.fn(),
+    onclick: jest.fn(),
+    onsubmit: jest.fn()
+    
 };
 
 //For test to pass you need to comment out console.log('Got response from server'); 
@@ -47,15 +58,125 @@ describe('Profile ', () => {
       let wrapper = shallow(<Profile {...props} />);
     });
 
-    it('should call onsubmit when submit', () => {
-      const form = mount<Profile>(<Profile {...props} />).find('form');
-      
-      const event = {
-        preventdefault: jest.fn()
+    it ('should return false', () => {
+      props.theBooking = {
+        view: 2,
+        guests: 4,
+        date: new Date(),
+        time: '18:00',
+        customerId: 5,
+        profile: {
+          firstName: 'Michael',
+          lastName: 'Persson',
+          email: 'Michaelperssonmail@gmail.com',
+          phone: '12345678',
+    
+          firstNameError: '',
+          lastNameError: '',
+          emailError: '',
+          phoneError: '',
+    
+          showFirstNameError: true,
+          showLastNameError: true,
+          showEmailError: true,
+          showPhoneError: true,
+    
+          myBookings: [],
+          myCustomers: []
+        }
       }
 
-      form.simulate('submit', event);
+      const wrapper = shallow<Profile>(<Profile {...props} />);
+      wrapper.instance().setState({
+        myCustomers: [{
+          id: 1,
+          firstName: 'Sebastian',
+          lastname: 'Tegel',
+          email:'sebastian.tegel@tegelconsulting.se',
+          phone: '0701231231'
+        }]
+      });
+      
+      let result = wrapper.instance().isCustomerInDatabase();
 
-      expect(props.onsubmit).toHaveBeenCalled();
-  });
+      expect(result).toBe(false);
+
+    })
+
+    it ('should return true', () => {
+      props.newCustomer = {
+        firstName: 'Sebastian',
+        lastName: 'Tegel',
+        email:'sebastian.tegel@tegelconsulting.se',
+        phone: '0701231231'
+      };
+
+      props.theBooking = {
+        view: 2,
+        guests: 4,
+        date: new Date(),
+        time: '18:00',
+        customerId: 5,
+        profile: {
+          firstName: 'Sebastian',
+          lastName: 'Tegel',
+          email: 'sebastian.tegel@tegelconsulting.se',
+          phone: '12345678',
+    
+          firstNameError: '',
+          lastNameError: '',
+          emailError: '',
+          phoneError: '',
+    
+          showFirstNameError: true,
+          showLastNameError: true,
+          showEmailError: true,
+          showPhoneError: true,
+    
+          myBookings: [],
+          myCustomers: []
+        }
+      }
+
+      const wrapper = shallow<Profile>(<Profile {...props} />);
+
+      wrapper.instance().setState({
+        myCustomers: [{
+          id: 1,
+          firstName: 'Sebastian',
+          lastname: 'Tegel',
+          email:'sebastian.tegel@tegelconsulting.se',
+          phone: '0701231231'
+        }]
+      });
+      wrapper.update();
+    
+      let result = wrapper.instance().isCustomerInDatabase();
+
+      expect(result).toBe(true);
+
+    })
+
+  //   it('should call handleSubmit when submit', async () => {
+  //     const wrapper = mount<Profile>(<Profile {...props} />);
+  //     let form = wrapper.find('form');
+
+  //     wrapper.instance().handelNewCustomer = jest.fn(() => { Promise.resolve({
+  //       data: { id: 1}
+  //     })});
+
+  //     const event = {
+  //       preventdefault: jest.fn()
+  //     }
+
+  //     let button = form.find('button');
+
+  //     await button.simulate('click', event);
+
+  //     wrapper.update();
+
+  //     expect(props.handleSubmit).toHaveBeenCalled();
+  // });
+
+
 });
